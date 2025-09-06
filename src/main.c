20b0a253a61705a9884a2d64d8592e1283237fde
Ofek Grunfeld <ofekgrun@gmail.com>
2025-09-06 20:56:14 +0300

    feat: Move connection functionality to a different file (net.c); 1. Create server socket 2. Receive connections

--- src/main.c
+++ src/main.c
@@ -3,11 +3,10 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <stdbool.h>
 
 #include "args.h"
 #include "echo.h"
+#include "net.h"
 
 #define LISTEN_BACKLOG 1
 #define CHUNK  65536
@@ -18,36 +17,18 @@ do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 int main(const int argc, const char *argv[]) {
     Args args;
-    if (parse_args(argc, argv, &args))
-        return EXIT_FAILURE;
+    if (parse_args(argc, argv, &args)) return EXIT_FAILURE;
 
-    socklen_t client_address_size;
-    struct sockaddr_in server_address = {0}, client_address = {0};
+    const int s_fd = net_listen_tcp_any(args.port, LISTEN_BACKLOG);
+    if (s_fd < 0) return EXIT_FAILURE;
 
-    const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0)
-        handle_error("socket");
+    const int c_fd = net_accept(s_fd);
+    if (c_fd < 0) return EXIT_FAILURE;
 
-    const bool one = true;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(args.port);
-
-    if (bind(server_fd, (struct sockaddr *) &server_address, sizeof(server_address)))
-        handle_error("bind");
-
-    if (listen(server_fd, LISTEN_BACKLOG))
-        handle_error("listen");
-
-    client_address_size = sizeof(client_address);
-    const int client_fd = accept(server_fd, (struct sockaddr *) &client_address, &client_address_size);
-    if (client_fd < 0)
-        handle_error("accept");
-
-    int rc = echo_splice(client_fd);
-    close(client_fd); close(server_fd);
+    const int rc = echo_splice(c_fd);
+    close(c_fd);
+    close(s_fd);
 
     return (rc == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
---
+++ src/net.c
@@ -0,0 +1,47 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "net.h"
+
+int net_listen_tcp_any(const uint16_t port, const int backlog) {
+    const int s_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (s_fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    const int enable = 1;
+    if (setsockopt(s_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+        handle_socket_error("setsockopt", s_fd);
+        return -1;
+    }
+
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    if (bind(s_fd, (struct sockaddr *) &addr, sizeof addr) < 0) {
+        handle_socket_error("bind", s_fd);
+        return -1;
+    }
+    if (listen(s_fd, backlog) < 0) {
+        handle_socket_error("listen", s_fd);
+        return -1;
+    }
+    return s_fd;
+}
+
+int net_accept(const int listen_fd) {
+    for (;;) {
+        const int c = accept(listen_fd, NULL, NULL);
+        if (c >= 0)
+            return c;
+        perror("accept");
+        return -1;
+    }
+}
